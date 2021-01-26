9b2dced9de3573dcda1004a47e0fcc711b01aac8
asheeraz <76551932+asheeraz@users.noreply.github.com>
2021-01-26 11:10:52 -0800

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **This is the first notebook of Applied Data Science Capstone Course for Peer-graded Assignment: Capstone Project Notebook of Week 1**
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
